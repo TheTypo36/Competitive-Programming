4bf841bec148d13c864be3e60c138a9a25161dee
Ashish Raghuvanshi <thetypo36@gmail.com>
2020-09-22 10:52:46 +0530

    watermelon*800

--- waterMelon.cpp
+++ waterMelon.cpp
@@ -5,9 +5,16 @@ int main(){
 
     int n;
    cin >> n;
-    if(n%2==0){
-        cout << "YES" << endl;
-    }else{
-        cout << "NO" << endl;
+    for (int i = 2; i < n-1; i++)
+    {
+        int a = n-i;
+        int b = i;
+        if(a%2==0&&b%2==0){
+            cout << "YES" << endl;
+            return 0;
+        }
     }
+    cout << " NO" << endl;
+    return 0;
+    
 }
