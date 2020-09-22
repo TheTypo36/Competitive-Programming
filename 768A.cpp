a0c930a4449da60da958bd554e9e741b3cf0e4a0
Ashish Raghuvanshi <thetypo36@gmail.com>
2020-09-22 18:23:37 +0530

    codeforces*900

--- 768A.cpp
+++ 768A.cpp
@@ -1,20 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
 
     int n;
-   cin >> n;
-    int * input = new int[n+1];
+    cin >> n;
+    int *input = new int[n + 1];
     for (int i = 0; i < n; i++)
     {
         cin >> input[i];
     }
-    unordered_set<int> s;
-    for (int i = 1; i < n-1; i++)
+    int mininum = INT_MAX;
+    int maximum = INT_MIN;
+    for (int i = 0; i < n; i++)
     {
-        s.insert(input[i]);
+        if (mininum > input[i])
+            mininum = input[i];
+        if (maximum < input[i])
+            maximum = input[i];
     }
-    cout << s.size() << endl;
-    
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (input[i] > mininum && input[i] < maximum)
+            count++;
+    }
+   // cout << maximum << endl;
+    //cout << mininum << endl;
+    cout << count << endl;
 }
