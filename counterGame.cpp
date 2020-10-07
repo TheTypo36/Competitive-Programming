b9da47067117a4453eb18272610928fb2434802a
Ashish Raghuvanshi <thetypo36@gmail.com>
2020-10-07 17:29:14 +0530

    Bit manipulation(Hackerrank)

---
+++ counterGame.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+string counterGame(long n) {
+	int i = 0;
+	
+
+	while(n!=1){
+		if((n&(n-1))==0){
+			n = n >> 1;
+
+		}else{
+			int m = n;
+			int c = 0;
+			while(m!=0){
+				m = m >> 1;
+				c++;
+			}
+			int z = (1 << (c-1));
+			//cout << z << endl;
+			n = n - z ;
+		}
+		i++;
+
+
+	}
+	if(i%2==0){
+		return "Richard" ;
+	}else{
+		return "Louise" ;
+	}
+
+
+}
+int main()
+{
+	int testcases;
+	cin >> testcases;
+	while(testcases--)
+	{
+		ll n;
+		cin >> n;
+		cout <<counterGame(n) << endl;
+	}
+	return 0;
+}
