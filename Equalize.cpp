3ab5861d508e5f4c7dde5e7e3db72db0907ca03f
Ashish Raghuvanshi <thetypo36@gmail.com>
2020-10-07 12:33:53 +0530

    Codeforces

---
+++ Equalize.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minCost(int n,string s1, string s2){
+	
+	int ans = 0;
+	for(int i = 0 ; i < n; i++){
+		if(s1[i]==s2[i]){
+			continue;
+		}else{
+			if(i < n-1 &&s1[i]!=s2[i]&&s1[i+1]!=s2[i+1]){
+				ans++;
+				i++;
+			}
+			else if(s1[i]!=s2[i]){
+				ans++;
+			}
+		}
+	}
+	return ans;
+}
+int main(){
+	int n;
+	cin >> n;
+	string s , s2;
+	cin >> s >> s2;
+	cout << minCost(n,s,s2) << endl;
+}
