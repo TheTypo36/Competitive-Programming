afe32c355e48d6d17573e688df11586e577fa7e6
Ashish Raghuvanshi <thetypo36@gmail.com>
2020-10-21 08:30:10 +0530

    DP (CN)

--- JohnSnowAndX.cpp
+++ JohnSnowAndX.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define size 1025
+#define size 1024
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -14,45 +14,41 @@ int main(){
 	 	rangers[strength]++;
 	}
 	vector<int> ans(size, 0);
-	for (int i = 0; i < k; ++i)
+	while(k--)
 	{
-		for (int i = 0; i <= 1024; ++i)
+		for (int i = 0; i < size; ++i)
 		{
 			ans[i] = 0;
 		}
 		int sum = 0;
 		int j;
-		for ( j = 0; j <= 1024; ++j)
+		for ( j = 0; j < size; ++j)
 		{
-			if (rangers[j]>0){
-                int index = j ^ x;
-                if (rangers[j] % 2 == 0){
-                    ans[index] += (rangers[j]/2);
-                    ans[j] += (rangers[j]/2);
-                }
-                else if(rangers[j] % 2 != 0){
-                    if (sum %2 == 0){
-                        ans[index] += ( (rangers[j] /2) + 1);
-                        ans[j] += (rangers[j] / 2);
-                    }else{
-                        ans[index]+= (rangers[j] /2);
-                        ans[j] += ((rangers[j]/2)+ 1);
-                    }
-                }
-                sum += rangers[j];
-                }
+			int units = rangers[j];
+			int odd ;
+			int even;
+				if(sum % 2 == 0){
+					 odd = (units+1)/2; 
+					even = units - odd;
+				}else{
+					 odd = (units/2);
+					even = units - odd;
+				}	
+			int index = x ^ j;
+			ans[index] += odd;
+			ans[j] += even;
+			sum +=units;
 		
 			}
-			for(int i = 0 ; i <= 1024; i++)
+			for(int i = 0 ; i < size; i++)
 			{
 				rangers[i] = ans[i];
 			}
 	}
 
-
 	for (int i = size-1; i >=0 ; --i)
 	{
-		if(ans[i] != 0){
+		if(rangers[i] != 0){
 			cout << i << " ";
 			break;
 		}	
