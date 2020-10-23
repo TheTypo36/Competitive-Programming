41a7a7ab368e6fdcc1a6a0e8c801e938277b227c
Ashish Raghuvanshi <thetypo36@gmail.com>
2020-10-23 10:16:35 +0530

    Dp coding ninjas

--- BalikAndVadhu.cpp
+++ BalikAndVadhu.cpp
@@ -1,53 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
-// int HappinessDp(string a, string b, int k){
-// 	int m = a.length();
-// 	int n = b.length();
-// 	vector<vector<vector<int>>> dp(m+1, vector<vector<int>>(n+1,vector<int>(k+1, 0)));
-// 	for (int i = 0; i <= m; ++i)
-// 	{
-// 		for (int j = 0; j <= n; ++j)
-// 		{
-// 			dp[i][j][0] = 0;
-// 		}
-// 	}
-// 	for (int i = 0; i <= m; ++i)
-// 	{
-// 		for (int j = 0; j <= n; ++j)
-// 		{
-// 			for (int p = 1; p <= k; ++p)
-// 			{
-// 				if(i== 0|| j == 0)
-// 				{
-// 					dp[i][j][p] = INT_MIN;
-// 				}
-// 			}
-// 		}
-// 	}
-// 	for (int i = 0; i <= m; ++i)
-// 	{
-// 		for (int j = 0; j <= n; ++j)
-// 		{
-// 			for (int p = 0; p <= k; ++p)
-// 			{
-// 				if(a[m-i] == b[n-i]){
-// 					int ascii = b[n-i];
-// 					dp[i][j][k] = max(ascii+dp[i-1][j-1][k-1],max(dp[i-1][j][k],dp[i][j-1][k]));
-// 				}else{
-// 					dp[i][j][k] = max(dp[i-1][j][k],dp[i][j-1][k]);
-// 				}
-// 			}
-// 		}
-// 	}
-// 	return dp[m][n][k];
-// }
+int HappinessDp(string a, string b, int k){
+	int m = a.length();
+	int n = b.length();
+	int*** dp = new int**[m+1];
+	for (int i = 0; i <= m+1; ++i)
+	{
+		dp[i] = new int*[n+1];
+		for (int j = 0; j <= n; ++j)
+		{
+			dp[i][j] = new int[k+1];
+		}
+	}
+	for (int i = 0; i <= m; ++i)
+	{
+		for (int j = 0; j <= n; ++j)
+		{
+			dp[i][j][0] = 0;
+		}
+	}
+	for (int i = 0; i <= m; ++i)
+	{
+		for (int j = 0; j <= n; ++j)
+		{
+			for (int p = 1; p <= k; ++p)
+			{
+				if(i== 0|| j == 0)
+				{
+					dp[i][j][p] = INT_MIN;
+				}
+			}
+		}
+	}
+	for (int i = 1; i <= m; ++i)
+	{
+		for (int j = 1; j <= n; ++j)
+		{
+			for (int p = 1; p <= k; ++p)
+			{
+				if(a[i-1] == b[j-1]){
+					int ascii = b[j-1];
+					dp[i][j][p] = max(ascii+dp[i-1][j-1][p-1],max(dp[i-1][j][p],dp[i][j-1][p]));
+				}else{
+					dp[i][j][p] = max(dp[i-1][j][p],dp[i][j-1][p]);
+				}
+			}
+		}
+	}
+	return dp[m][n][k];
+}
 int Happiness(string a, string b, int k,int*** dp)
 {
 
 	if(k == 0){
 		return 0;
 	}
-	if(a.length() ==0 || b.length() ==0 ){
+	if(a.length() == 0 || b.length() == 0 ){
 		return INT_MIN;
 	}
 	if(dp[a.length()][b.length()][k] != -1){
@@ -98,7 +106,7 @@ int main(){
                 }
             }
         }
-		ans = Happiness(a,b,k,dp);
+		 ans = HappinessDp(a,b,k);
 		if(ans <0)
 			cout << 0 << endl;
 		else
