5c3fe47d289b9f3f0fee6001ccb6f59534f992c0
Ashish Raghuvanshi <thetypo36@gmail.com>
2020-10-26 15:41:23 +0530

    dp CN

--- WiserMan.cpp
+++ WiserMan.cpp
@@ -1,21 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int total_min_fare(int** strg,int row, int col, int n, int m)
+int total_min_fare(int** strg,int row, int col, int n, int m,int ** dp)
 {
-	if(row >= n || col >= m){
-		return INT_MAX;
+	
+	if(row == n){
+		return 0;
 	}
-	int option1 = total_min_fare(strg,row,col+1,n,m);
-	int Curr_fare = strg[row][col];
-	int option2 = INT_MAX;
-	for (int i = col; i < m; ++i)
+	if(dp[row][col] != -1)
 	{
-		if(Curr_fare == strg[row+1][i] || Curr_fare - 1 == strg[row+1][i] || Curr_fare + 1 == strg[row+1][i]){
-			 option2 = min(option2,total_min_fare(strg,row+1,i,n,m));
-		}
+		return dp[row][col];
 	}
-	return min(option1,Curr_fare+option2);
+
+	int option2 = strg[row][col]+total_min_fare(strg,row+1,col,n,m,dp);
+	int option1 = INT_MAX;
+	if(col+1 < n){
+		option1 = strg[row][col]+total_min_fare(strg,row+1,col+1,n,m,dp);
+	}
+	int option3 = INT_MAX;
+	if(col - 1 >=0){
+	 	option3 = strg[row][col]+total_min_fare(strg,row+1,col-1,n,m,dp);
+	}
+	return dp[row][col] = min(option2,min(option3,option1));
 }
 int main(){
 	ios_base::sync_with_stdio(false);
@@ -33,5 +39,25 @@ int main(){
 			cin >> strg[i][j];
 		}
 	}
-	cout << total_min_fare(strg,0,0,n,m) << endl;
+	int min_ans = INT_MAX;
+	int** dp = new int*[n+1];
+		for (int i = 0; i < n+1; ++i)
+		{
+			dp[i] = new int[m+1];
+			
+		}
+	for (int i = 0; i < m; ++i)
+	{
+		
+		for (int i = 0; i < n+1; ++i)
+		{
+			for (int j = 0; j < m+1; ++j)
+			{
+				dp[i][j] = -1;				
+			}
+		}
+		min_ans = min(min_ans,total_min_fare(strg,0,i,n,m,dp));
+	}
+	cout << min_ans << endl;
+	return 0;
 }
