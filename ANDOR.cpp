e0d6948b8a2b0a28e15dab7a0b4dde80e51e92eb
Ashish Raghuvanshi <thetypo36@gmail.com>
2020-11-01 09:15:28 +0530

    Codechef Nov Lunctime A

---
+++ ANDOR.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	int testcases;
+	cin >> testcases;
+	while(testcases--)
+	{
+		ll x;
+		cin >> x;
+		ll a = x;
+		ll b = 0;
+		cout << a << " " << b << endl;
+	}
+	return 0;
+}
